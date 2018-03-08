dbe47342c6e147d2a9196f23a8797d4ce1e3c537
ofthestorm <870771017@qq.com>
2018-03-08 16:28:47 +0800

    online & offline broadcast

--- index.js
+++ index.js
@@ -10,17 +10,10 @@ var bodyParser = require('body-parser');
 app.use(cookieParser());
 app.use(bodyParser());
 
-
-var users = new Array();
+//online users
+var users = new Set();
 
 app.get('/', function (req, res) {
-    // res.send('Hello World!');
-    // res.sendFile(__dirname + '/src/index.html');
-    // res.cookie("name", "hello");
-
-    // console.log('Cookies: ', req.cookies);
-    // console.log('Signed Cookies: ', req.signedCookies);
-
     if (req.cookies.name == null) {
         res.redirect('/signin');
     } else {
@@ -29,14 +22,19 @@ app.get('/', function (req, res) {
 });
 
 app.get('/signin', function (req, res) {
-    res.sendFile(__dirname + '/src/signin.html');
-    console.log(users);
+    if (req.cookies.name == null) {
+        res.sendFile(__dirname + '/src/signin.html');
+    } else {
+        res.redirect('/');
+    }
+    // console.log(users);
     // console.log("cookie:"+req.cookies.name)
 });
 
 app.post('/signin', function (req, res) {
 
-    if (users.indexOf(req.cookies.name) != -1) {
+    // if (users.indexOf(req.cookies.name) != -1) {
+    if (users.has(req.cookies.name)) {
         //存在，则不允许登陆
         res.redirect('/signin');
         // console.log("exist");
@@ -45,40 +43,44 @@ app.post('/signin', function (req, res) {
         // console.log("not exist");
         console.log("cookie:"+req.body.name);
         res.cookie('name', req.body.name, {domain: 'localhost',maxAge: 1000*60*60*24*30});
-        users.push(req.body.name);
+        users.add(req.body.name);
         res.redirect('/');
     }
 });
 
 io.on('connection', function(socket){
     // console.log('connected');
+    
+    //https://stackoverflow.com/questions/40816355/socket-io-send-disconnect-event-with-parameter
     socket.on('disconnect', function(){
-        console.log(socket.name+'disconnected');
-        if (users.indexOf(socket.name) != -1) {
+        // console.log(socket.name+' disconnected');
+        if (users.has(socket.name)) {
             //从 users 对象中删除该用户名
-            //todo: socket.name --> hello 与数组中不一致
-            delete users[socket.name];
-            console.log(users);
+            users.delete(socket.name);
             //向其他所有用户广播该用户下线信息
             io.emit('offline', {users: users, user: socket.name});
         }
     });
+
     socket.on('chat message', function(msg, name){
         // console.log('message: ' + msg + name);
         io.emit('chat message', msg, name); //广播
     });
+
     socket.on('online', function(data){
-        console.log(data.user+"login");
+        // console.log(data.user+" login");
         socket.name = data.user;
-        console.log("socket name:"+socket.name);
-        io.emit('online', data);
+        // console.log("socket name:"+socket.name);
+        users.add(data.user);
+        console.log(users);
+
+        //fixme： users无法传递
+        // io.emit('online', {users: users, user: socket.name});
+        io.emit('online', {users: users, user: socket.name});
+
     });
 });
 
-// io.on('connection', function(socket){
-//     socket.broadcast.emit('chat message');
-// });
-
 http.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Listening on port 3000');
 });
