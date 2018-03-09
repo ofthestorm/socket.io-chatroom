da004ff4a4b5683d9f23d7c5038b4839c782f7e3
ofthestorm <870771017@qq.com>
2018-03-09 11:32:44 +0800

    sign in

--- index.js
+++ index.js
@@ -27,21 +27,13 @@ app.get('/signin', function (req, res) {
     } else {
         res.redirect('/');
     }
-    // console.log(users);
-    // console.log("cookie:"+req.cookies.name)
 });
 
 app.post('/signin', function (req, res) {
 
-    // if (users.indexOf(req.cookies.name) != -1) {
     if (users.has(req.cookies.name)) {
-        //存在，则不允许登陆
         res.redirect('/signin');
-        // console.log("exist");
     } else {
-        //不存在，把用户名存入 cookie 并跳转到主页
-        // console.log("not exist");
-        console.log("cookie:"+req.body.name);
         res.cookie('name', req.body.name, {domain: 'localhost',maxAge: 1000*60*60*24*30});
         users.add(req.body.name);
         res.redirect('/');
@@ -49,15 +41,11 @@ app.post('/signin', function (req, res) {
 });
 
 io.on('connection', function(socket){
-    // console.log('connected');
 
     //https://stackoverflow.com/questions/40816355/socket-io-send-disconnect-event-with-parameter
     socket.on('disconnect', function(){
-        // console.log(socket.name+' disconnected');
         if (users.has(socket.name)) {
-            //从 users 对象中删除该用户名
             users.delete(socket.name);
-            //向其他所有用户广播该用户下线信息
             io.emit('offline', {users: users, user: socket.name});
         }
     });
@@ -68,12 +56,10 @@ io.on('connection', function(socket){
 
     socket.on('online', function(data){
         socket.name = data.user;
-        // console.log("socket name:"+socket.name);
         users.add(data.user);
         console.log(users);
 
         //fixme： users无法传递
-        // io.emit('online', {users: users, user: socket.name});
         io.emit('online', {users: users, user: socket.name});
 
     });
